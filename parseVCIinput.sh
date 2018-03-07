ad56f5413aa0cd6bf5e454d8397b051684f45a8d
Larry Babb <larry.babb@geneinsight.com>
2018-03-07 08:53:17 -0500

    final cleanup and run from first PAH set of VCI records

--- parseVCIinput.sh
+++ parseVCIinput.sh
@@ -3,42 +3,54 @@
 # this unzips a file that is presumed to contain a single json file of the same name as the zip (without .zip extension)
 # this does NOT have error handling for any missing or overwriting of files. 
 # it does not take into consideration that other files with the same names may end up getting processed in secondary steps
-i
+
+# temp file is used to support counter
 TEMPFILE=/tmp/$$.tmp
 echo 0 > $TEMPFILE
 
+# unzip the zipped json file containing the vci records and split into separate files with prefix 'vci'
 zipfile=$1
 jsonfile=$(echo ${zipfile%.*})
 splitfileprefix="vci"
-
 unzip $zipfile $jsonfile
 gcsplit --digits=4 --prefix=$splitfileprefix $jsonfile "/^    },$/" "{*}"
 
-# Loop through product of gcsplit operation above...
-for filename in ${splitfileprevix}*; do
-    printf "%s\n" "$filename"
-    testIt=$(head -3 "$filename" | pcregrep -M '},\n    {\n        \"modeIn')
-    if [ -z $testIt ]; then
-       echo -n ""
-    else
-       # Fetch the counter value and increase it
-       counter=$[$(cat $TEMPFILE) + 1]
+# declare and capture the files to process in an array
+declare -a files
+files=(vci*)
+pos=$(( ${#files[*]} - 1 ))
+last=${files[$pos]}
 
-       # Store the new value
-       echo $counter > $TEMPFILE
+# loop through the vci* files, prep and process to dmwg#.json form...
+for FILE in "${files[@]}"
+do 
 
-       #define the vcixxx.json input filename and dmwgxxx.json output filenames
-       input=${filename}.json
-       output="dmwg${counter}.json"
-       
-       # fix split files by removing the first line and adding the closing paren at the end.
-       sed '1d' "$filename" > tmpfile; mv tmpfile "$input"; echo "    }" >> "$input"
-       printf "%s\n" "Transforming $input to $output"
-       
-       # transform the vci json to dmwg using project's python script
-       python ../VCI2DMWG.py $input $output 
-    fi
-done
+  # output the filename being processed
+  printf "%s\n" "$FILE"
+
+  # Fetch, increase and store the counter
+  counter=$[$(cat $TEMPFILE) + 1]
+  echo $counter > $TEMPFILE
+
+  # define the vcixxx.json input filename and dmwgxxx.json output filenames
+  input=${FILE}.json
+  output="dmwg${counter}.json"
+
+  # The last file split out contains an additional 2 lines at the end 
+  # which must be removed before continuing
+  if [[ $FILE == $last ]]
+  then
+   sed -e :a -e '$d;N;2,2ba' -e 'P;D' "$FILE" > tmpfile; mv tmpfile "$FILE"
+  fi
+
+  # fix split files by removing the first line and adding the closing paren at the end.
+  sed '1d' "$FILE" > tmpfile; mv tmpfile "$input"; echo "    }" >> "$input"
+  printf "%s\n" "Transforming $input to $output"
+
+  # transform the vci json to dmwg using project's python script
+  python ../VCI2DMWG.py $input $output
+
+done 
 
 # delete the tempfile
 unlink $TEMPFILE
